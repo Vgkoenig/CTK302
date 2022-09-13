8681eda228eb7aaa73fe5ca7b393ae75ec33e76c
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-13 03:04:07 -0500

    assignments

---
+++ Unit1/fonts/sketch.js
@@ -0,0 +1,13 @@
+let f1, f2, f3;
+
+function setup() {
+  createCanvas(500, 500);
+  f1 = loadFont("assets/handwriting.ttf");
+  f2 = loadFont("assets/redacted.tff");
+  f3 = loadFont("assets/moon.ttf");
+
+}
+
+function draw() {
+
+}
