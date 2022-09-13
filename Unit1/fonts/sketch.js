f798fa5869bc28579b89873f3fd637ae13c6ba1f
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-13 03:04:21 -0500

    assignment

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -2,12 +2,19 @@ let f1, f2, f3;
 
 function setup() {
   createCanvas(500, 500);
-  f1 = loadFont("assets/handwriting.ttf");
-  f2 = loadFont("assets/redacted.tff");
-  f3 = loadFont("assets/moon.ttf");
-
+  f1 = loadFont("assets/hand/handwriting.ttf");
+  f2 = loadFont("assets/red/redacted.tff");
+  f3 = loadFont("assets/moon/moon.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
-
+  background("black");
+  fill("white");
+  textFont(f1, 48);
+  text("hello there!", width / 2, 100);
+  textFont(f2, 24);
+  text("hello there!", width / 2, 200);
+  textFont(f3, 48);
+  text("hello there!", width / 2, 300);
 }
