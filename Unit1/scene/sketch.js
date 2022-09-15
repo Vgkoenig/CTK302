661eaee447d5c52600a620383c05662ec06be7b1
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-15 11:49:04 -0500

    assignment

---
+++ Unit1/scene/sketch.js
@@ -0,0 +1,36 @@
+function setup(){
+  createCanvas(720, 400);}
+
+function draw() {
+
+  background("pink");
+  noStroke();
+
+  fill("rgb(249,189,78)");
+  triangle(384, 33, 574, 343, 630, 292);
+  
+  fill("rgb(255,245,249)")
+  ellipse(130, 154, 100, 100)
+  
+  fill("rgb(255,245,249)")
+  ellipse(159, 105, 100, 100)
+  
+  fill("rgb(255,245,249)")
+  ellipse(208, 157, 100, 100)
+  
+   fill("rgb(255,245,249)")
+  ellipse(279, 157, 100, 100)
+  
+   fill("rgb(255,245,249)")
+  ellipse(233, 115, 100, 100)
+  
+  fill("rgb(241,166,27)");
+  triangle(291, 393, 576, 367, 570, 417 );
+
+  fill("rgb(241,166,27)");
+  triangle(700, 18, 770, 365, 663, 365);
+
+  fill("rgb(235,104,9)");
+  arc(680, 420, 300, 300, PI, TWO_PI);
+  
+}
