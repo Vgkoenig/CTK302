44a800462e5c1ad273a998d1c29b46b9fb9622f3
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-27 11:34:36 -0500

    y

--- Unit1/joke2/sketch.js
+++ Unit1/joke2/sketch.js
@@ -1,7 +1,32 @@
+let state = 0;
+let timer = 0; 
 function setup() {
   createCanvas(500, 500);
+  textAlign(CENTER);
 }
 
 function draw() {
 
+  switch(state){
+    case 0:
+      background("red");
+      text("why did \nthe chx", width / 2, height / 2); 
+    break;
+
+    case 1:
+      background("blue");
+      text("because", width / 2, height / 2);
+    break;
+  }
+
+    timer++ ;
+    if (timer > 4 * 60) {
+    timer = 0 ;
+    state++;
+    if (state > 1) {
+      state = 0;
+    }
+  }
 }
+
+
