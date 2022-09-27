ffad78b31818560b6b45e57bf921b449ade73af8
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-27 11:24:24 -0500

    edit

---
+++ Unit1/joke1/sketch.js
@@ -0,0 +1,20 @@
+let state = 0;
+
+function setup() {
+  createCanvas(500, 500);
+  rectMode(CENTER);
+}
+
+function draw() {
+
+  switch(state){
+    case 0:
+      background("red");
+      text("why did the chx", width / 2, height / 2, 400, 400); 
+    break;
+
+    case 1:
+    break:
+
+  }
+}
