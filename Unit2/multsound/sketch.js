4d78aa891c5d256a402b578e9735e176e6d54289
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-09-29 11:35:50 -0500

    s

---
+++ Unit2/multsound/sketch.js
@@ -0,0 +1,54 @@
+let state = 0;
+
+let s1, s2, s3;
+function setup() {
+  createCanvas(800, 800);
+
+  function preload(){
+    s1 = loadSound("assets/funky.mp3");
+    s2 = loadSound("assets/photo.mp3");
+    s3 = loadSound("assets/piano.mp3");
+  }
+
+function draw() {
+
+  background(100);
+  
+  switch (state) {
+
+    case 0:
+      text("0", 100, 100);
+      if (!s1.isPlaying()){
+        s1.play();
+      }
+      break;
+
+    case 1:
+      text("1", 100, 100);
+      if (!s2.isPlaying()){
+        s2.play();
+      }
+      break;
+
+    case 2:
+      text("2", 100, 100);
+      if (!s3.isPlaying()){
+        s3.play();
+      }
+      break;
+
+  }
+}
+
+function mouseReleased() {
+  s1.stop();
+  s2.stop();
+  s3.stop();
+  state++;
+  if (state > 2) state = 0;
+
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
