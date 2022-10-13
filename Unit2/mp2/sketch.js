36a9bf1ef251897f8bfdb5273ad37dbbd0e25748
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-10-12 21:13:50 -0500

    submit

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,6 +1,9 @@
 let state = 0;
 let img;
-//let s1;
+let s1;
+let myFont;
+let timer = 0;
+
 
 function preload() {
   img = loadImage('assets/Entrance.jpg');
@@ -8,30 +11,33 @@ function preload() {
   img2 = loadImage('assets/breakrock.png');
   img3 = loadImage('assets/norock.PNG');
   img4 = loadImage('assets/treasure.png');
- // s1 = loadSound("assets/cavemusic.mp3");
+  img5 = loadImage('assets/end.png');
+  s1 = loadSound("assets/cavemusic.mp3");
+  myFont = loadFont('assets/Minecraft.ttf');
 
 }
 
-   // Top-left corner of the img is at (10, 10)
-  // Width and height are 50×50
-  // image(img, 10, 10, 50, 50);
-
+   
 function setup() {
   createCanvas(1540, 500);
-  //if (!s1.isPlaying()){
-    //s1.play();
+  if (!s1.isPlaying()){
+  s1.play();
+  }
+  //  that music was made by walkabout on youtube
 }
 
 function draw() {
   background(220);
   
-  }
+
 
   switch (state) {
     case 0:
       background("rgba(13,7,37,255)")
       fill("rgb(204, 216, 240)");
-      text("in state 0", 100, 100);
+      textFont(myFont)
+      textSize(20);
+      text("Cave Explorers!\n   Click to Start", 100, 100);
       image(img, 300, 50, 1000, 400);
 
       break;
@@ -39,7 +45,7 @@ function draw() {
       case 1:
       background("rgba(13,7,37,255)")
       fill("rgb(204, 216, 240)");
-      text("in state 1", 100, 100);
+      text("                 oh no!\nRocks have blocked the path!\n   *Click to break the rocks*", 10, 100);
       image(img1, 300, 50, 1000, 400);
 
       break;
@@ -47,7 +53,7 @@ function draw() {
       case 2:
       background("rgba(13,7,37,255)");
       fill("rgb(204, 216, 240)");
-      text("in state 2", 100, 100);
+      text("Almost there!", 100, 100);
       image(img2, 300, 50, 1000, 400);
 
       break; 
@@ -55,7 +61,7 @@ function draw() {
       case 3:
       background("rgba(13,7,37,255)");
       fill("rgb(204, 216, 240)");
-      text("in state 3", 100, 100);
+      text("Lets finish exploring...", 50, 100);
       image(img3, 300, 50, 1000, 400);
 
       break; 
@@ -63,16 +69,31 @@ function draw() {
       case 4:
       background("rgba(13,7,37,255)");
       fill("rgb(204, 216, 240)");
-      text("in state 4", 100, 100);
+      text("You've found\nThe treasure!", 100, 100);
       image(img4, 300, 50, 1000, 400);
+      timer++;
+      if (timer > 3 * 60) {
+        timer = 0;
+        state = 5;
+      }
 
+      break; 
+
+      case 5:
+      background("rgba(13,7,37,255)");
+      image(img5, 300, 50, 1000, 400);
+      timer++;
+      if (timer > 3 * 60) {
+        timer = 0;
+        state = 0;
+      }
       break; 
   }
 }
   
  function mouseReleased(){
    state++ ;
-   if (state > 4) {
+   if (state > 5) {
      state = 0;
    }
  }
