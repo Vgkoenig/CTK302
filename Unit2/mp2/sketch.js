4eb9fc25593176f63afa70b465fe39692e757e7d
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-10-12 19:44:26 -0500

    y

---
+++ Unit2/mp2/sketch.js
@@ -0,0 +1,33 @@
+let state = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  switch (state) {
+    case 0:
+      background("rgb(107,14,14)")
+      text("in state 0", 100, 100);
+      break;
+      
+      case 1:
+      background("rgb(192,163,192)")
+      text("in state 1", 100, 100);
+      break;
+      
+      case 2:
+      background("rgb(235,149,44)")
+      text("in state 2", 100, 100);
+      break; 
+  }
+}
+  
+ function mouseReleased(){
+   state++ ;
+   if (state > 2) {
+     state = 0;
+   }
+ }
