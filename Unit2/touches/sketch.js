a7e94df4d85bc28f3e96283b4b66caa9539f4348
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-10-13 00:09:15 -0500

    yes

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,5 +1,12 @@
 let numberOfTouches ;
+let img, img1, img2;
 
+function preload() {
+  img = loadImage('assets/ch1.jpeg');
+  img1 = loadImage('assets/ch2.jpeg');
+  img2 = loadImage('assets/ch3.jpeg');
+  img3 = loadImage('assets/ch4.jpeg');
+}
 function setup() {
   createCanvas(400, 400);
 }
@@ -11,22 +18,23 @@ function draw() {
   
   switch(numberOfTouches) {
     case 0: 
-      text("no one is touching the screen", 5, 22) ; 
+      text("Charlie is exploring peacfully", 5, 22) ;
+      image(img, 50, 50, 200, 250); 
       break ;
       
       case 1: 
-       text("it's kind of lonely here", 5, 22) ; 
-      // put a picture here
-      break ;
+       text("why would you disturb his adventures?", 5, 22) ; 
+       image(img1, 50, 50, 200, 250); 
+       break ;
       
       case 2:
-      text("two fingers are touching the screen", 5, 22) ; 
-            // put a picture here
+      text("now he attacks", 5, 22) ; 
+      image(img2, 50, 50, 200, 250); 
       break ;
       
       case 3:
-     text("three are touching the screen", 5, 22) ; 
-            // put a picture here
+     text("nvm he got distracted", 5, 22) ; 
+     image(img3, 50, 50, 200, 250); 
       break ;
     
       
