9559f26debb0d78381113521629c1f4e30e560b3
Vgkoenig <95943344+Vgkoenig@users.noreply.github.com>
2022-12-11 23:37:20 -0600

    font

--- unit4/gyro/sketch.js
+++ unit4/gyro/sketch.js
@@ -1,165 +1,89 @@
-// variables needed for gyroscope
-var beta, gamma; // orientation data
-var x = 0; // acceleration data
-var y = 0;
-var z = 0;
-var xPosition = 0;
-var yPosition = 0;
+/* For mobile phones - accesses accelerometer and gyroscope.
+Make sure you turn on orientation lock on your iPhone or Android device. */
+let alpha = 0, beta = 0 , gamma = 0; // gyroscope variablers
+let bunnyImage;
+let xPosition = 0;
+let yPosition = 0;
+let x = 0, y = 0, z = 0 ; // accelerometer data
 
-// var bunnyImage;
-var cars = [];
-var frogPos;
+function preload(){
+  myFont = loadFont('assets/MarkoftheBeastBB.ttf');
 
+}
 
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
-  // initialize accelerometer variables
-  alpha = 0;
-  beta = 0;
-  gamma = 0;
 
 
-  // spawn a bunch of cars
-  for (var i = 0; i < 40; i++) {
-    cars.push(new Car());
-  }
+  bunnyImage = loadImage("assets/boat.png");
 
-  // initialize the frog's position
-  frogPos = createVector(width / 2, height - 80);
-
-  // load any images you need
-  //bunnyImage = loadImage("assets/bunny.jpg");
   imageMode(CENTER);
   rectMode(CENTER);
-  noStroke();
+
 }
 
 function draw() {
+  background('#1F4C8D'); // light blue
 
-  background('#c6f5ff'); // light blue
-
-  // the map command !!!!
+  // the map c#3064AF!!!!
   // takes your variable and maps it from range 1 to range 2
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
-  xPosition = map(gamma, -18, 18, 0, width);
-  yPosition = map(beta, 25, 45, 0, height);
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
 
-
-  // move the frog around the screen
   push(); // before you use translate, rotate, or scale commands, push and then pop after
-  translate(xPosition, yPosition); // move everything over by x, y
-  //  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
 
-  // draw the FROG
-  // image(bunnyImage, 0, 0, 500, 500);
-  fill('green');
-  ellipse(0, 0, 80, 80);
+  translate(xPosition, yPosition); // move everything over by x, y
+
+  rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
+
+  image(bunnyImage, 0, 0, 500, 500);
+  // rect(0, 0, 100, 100) ;
   pop();
 
 
-  // update the frog's position using the accelerometer data
-  frogPos.x = xPosition;
-  frogPos.y = yPosition;
+  // Text commands that display debugging data
+  // textAlign(LEFT);
+  // textSize(20);
+  // fill('black');
+  // text("orientation data:", 25, 25);
+  // textSize(15);
+  // text("alpha: " + alpha, 25, 50);
+  // text("beta: " + beta, 25, 70);
+  // text("gamma: " + gamma, 25, 90);
+  // textSize(20);
+  // text("acceleration data:", 25, 125);
+  // textSize(15);
+  // text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  // text("y = " + y.toFixed(2), 25, 170);
+  // text("z = " + z.toFixed(4), 25, 190);
 
-  // iterate through the car loop to move them and see if we need to delete cars
-  for (var i = 0; i < cars.length; i++) {
-    cars[i].display();
-    cars[i].drive();
-    if (cars[i].pos.dist(frogPos) < 50) {
-      cars.splice(i, 1);
-    }
-  }
-
-  // MORE DECORATIONS - write that pretty ATK type on top.
-  fill('white');
-  textSize(40);
+  // Text that makes CTK type in the background
+  fill('rgb(23,78,144)');
+  stroke("white");
+  strokeWeight(2);
+  textFont(myFont);
+  textSize(50);
   textAlign(CENTER);
-  text("your words or image here!", width / 2, 600, windowWidth - 200, windowHeight - 200);
-
-
-  // Debugging information -- take this out when you're ready for production!
-  // Just a bunch of text commands to display data coming in from addEventListeners
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y, 25, 170);
-  text("z = " + z, 25, 190);
-
+  text("Watch out\nfor iceburgs!", width / 2, height / 2);
 
 }
 
-function deviceShaken() {
-  // re-spawn cars
-  cars = []; // clear the array first
-  for (var i = 0; i < 40; i++) {
-    cars.push(new Car());
-  }
-}
 
 
-// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
-
-// Read in accelerometer data
+// Read in gyroscope data
 window.addEventListener('deviceorientation', function(e) {
   alpha = e.alpha;
   beta = e.beta;
   gamma = e.gamma;
 });
 
-
-// accelerometer Data
+// Read in accelerometer data
 window.addEventListener('devicemotion', function(e) {
   // get accelerometer values
   x = e.acceleration.x;
   y = e.acceleration.y;
   z = e.acceleration.z;
 });
-
-
-
-
-
-// car class!!
-function Car() {
-  // attributes
-  this.pos = createVector(100, 100);
-  this.vel = createVector(random(-5, 5), random(-5, 5));
-  this.r = random(255);
-  this.g = random(255);
-  this.b = random(255);
-  this.a = random(255);  // alpha opacity value for fill!
-
-
-  // methods
-  this.display = function() {
-
-    // maybe use an image here instead!
-    fill(this.r, this.g, this.b, this.a);
-    ellipse(this.pos.x - 50, this.pos.y, 50, 50);
-    ellipse(this.pos.x + 50, this.pos.y, 50, 50);
-    rect(this.pos.x + 17, this.pos.y - 30, 80, 60) ;
-
-  }
-
-  this.drive = function() {
-    this.pos.add(this.vel);
-
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-
-  }
-
-}
